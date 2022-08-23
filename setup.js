c00c23e5377ba3e32000c8c458050beeaaa3d0f7
William Antônio Siqueira <william.fatecsjc@gmail.com>
2022-08-23 17:34:19 -0300

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,5 +1,5 @@
 dashbuilder = {
     hideNavBar: true,
     path: 'dashboards',
-    dashboards: ["Mortes SJC.yml", "preco_alimentos.dash.yml", "IDEB x FUNDEB.dash.yaml", "Orçamento União.dash.yml"]
+    dashboards: ["Mortes SJC.yml", "preco_alimentos.dash.yml", "IDEB x FUNDEB.dash.yaml", "Orçamento União.dash.yml", "Frota Veículos.dash.yaml"]
 }
